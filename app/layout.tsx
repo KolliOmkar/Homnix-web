4bbe2357ddee64451e15e0e288caae337d4d051b
KolliOmkar <kolliomkar0@gmail.com>
2026-05-08 02:23:52 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Homnix",
+  description: "Homnix Services",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
